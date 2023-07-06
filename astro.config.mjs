949495962025a15e954215383bc63159e7c65d33
Nolan Poe <npgo22@gmail.com>
2023-07-05 22:16:43 -0700

    feat: Conventional commits, dyanmically create robots.txt, SEO plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   site: 'https://nolanpoe.me',
-  integrations: [tailwind(), sitemap(), compress()]
+  integrations: [tailwind(), sitemap(), compress(), robotsTxt()]
 });
